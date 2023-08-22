e406780cc54bd37cd00d4b38b3cb94898b8883d0
Prathamesh Powar <ppowar1910@gmail.com>
2023-08-22 22:46:03 +0530

    Create bestTimeToBuyAndSellStock.cpp

---
+++ C++/bestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,16 @@
+//time O(n) space O(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxP = 0, l = 0, r = 0;
+        while (r < prices.size()){
+            if (prices[r] > prices[l])
+                maxP = max(maxP, prices[r] - prices[l]);
+            else
+                l = r;
+            ++r;
+        }
+        return maxP;
+    }
+};
