cbe0e8e1c4c959549bc159f6f72ccf71d32d28c7
Prathamesh Powar <ppowar1910@gmail.com>
2023-12-19 19:37:23 +0530

    Create searchInsertPosition.java

---
+++ Java/searchInsertPosition.java
@@ -0,0 +1,19 @@
+// time : O(logn)
+// space : O(1)
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int l = 0 , r = numsLen-1;
+       while (l<=r) {
+          int mid = l+ (r-l)/2;
+           if (nums[mid] == target) {
+               return mid;
+           } else if( nums[mid] > target ) {
+               r = mid - 1;
+           } else {
+               l = mid + 1;
+           }
+       }
+       return l;
+    }
+}
