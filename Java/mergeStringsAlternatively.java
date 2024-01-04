c2ae5faffa3f473eaef40e74c5db1bffb028e579
Prathamesh Powar <ppowar1910@gmail.com>
2024-01-04 20:00:16 +0530

    Update mergeStringsAlternatively.java

--- Java/mergeStringsAlternatively.java
+++ Java/mergeStringsAlternatively.java
@@ -4,15 +4,16 @@ class Solution {
         String merged = "";
         int length1 = word1.length();
         int length2 = word2.length();
-        int largerLength = (length1 >= length2) ? length1 : length2;
-
-        for (int i = 0 ; i < largerLength ; i++ ) {
+        
+        int i = 0;
+        while (i < length1 || i < length2) {
             if (length1 > i) {
                 merged += word1.charAt(i) ; 
             }
             if (length2 > i) {
                 merged += word2.charAt(i) ; 
             }
+            i++;
         }
 
         return merged;
