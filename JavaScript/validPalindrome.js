afb3f352b5ab920bcb12a588ac2bb369bddf7df2
Prathamesh Powar <ppowar1910@gmail.com>
2024-01-26 12:54:16 +0530

    Update validPalindrome.js

--- JavaScript/validPalindrome.js
+++ JavaScript/validPalindrome.js
@@ -1,1 +1,10 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+    const reverseS = s.split("").reverse().join("")
 
+    return s == reverseS
+};
