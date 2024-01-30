a63f5d27bc3dd91b998d5c49a2b52914b05684a4
Prathamesh Powar <ppowar1910@gmail.com>
2024-01-30 08:54:23 +0530

    Update twoSumArrayIsSorted.java

--- Java/twoSumArrayIsSorted.java
+++ Java/twoSumArrayIsSorted.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        int numLeft,numRight;
 
+        while (left<right) {
+            numLeft = numbers[left];
+            numRight = numbers[right];
+
+            if (numLeft + numRight == target) break ;
+
+            if (numLeft + numRight < target) {
+                left++;
+                continue;
+            }
+            right--;
+        }
+
+        return new int[] { left+1, right+1};
+    }
+}
