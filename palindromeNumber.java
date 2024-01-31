54ccb7dbf61fa37cdad3410ab22c2eac86448a9c
Prathamesh Powar <ppowar1910@gmail.com>
2024-01-31 09:02:31 +0530

    Create palindromeNumber.java

---
+++ palindromeNumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int result = 0;
+        int original = x;
+        while (x>0) {
+            result = result*10 +  x%10;
+            x /= 10;
+        }
+
+        return result == original;
+    }
+}
