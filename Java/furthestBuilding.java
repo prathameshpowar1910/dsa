0e8cf840f388e368b672dfa4e7eee9e891376fd9
Prathamesh Powar <ppowar1910@gmail.com>
2024-02-17 18:37:31 +0530

    Create furthestBuilding.java

---
+++ Java/furthestBuilding.java
@@ -0,0 +1,31 @@
+//PROBLEM NO 1642 LEETCODE
+//used greedy approach with min heap as here normal greedy fails as well as dp fails
+
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> p = new PriorityQueue<>((a, b) -> b - a);
+        
+        int i = 0, diff = 0;
+        for (i = 0; i < heights.length - 1; i++) {
+            diff = heights[i + 1] - heights[i];
+            
+            if (diff <= 0) {
+                continue;
+            }
+
+            bricks -= diff;
+            p.offer(diff);
+
+            if (bricks < 0) {
+                bricks += p.poll();
+                ladders--;
+            }
+
+            if (ladders < 0) {
+                break;
+            }
+        }
+        
+        return i;
+    }
+}
