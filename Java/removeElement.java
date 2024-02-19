e3c08713f125a51c67bb8b64e1928757b05d2d4f
Prathamesh Powar <ppowar1910@gmail.com>
2024-02-19 22:46:58 +0530

    Create removeElement.java

---
+++ Java/removeElement.java
@@ -0,0 +1,15 @@
+//PROBLEM NO 27 LEETCODE
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0 ; i<nums.length ; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+}
